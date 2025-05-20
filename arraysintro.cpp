f59b56486e6ec676239070231fdef603da4d3d84
Radhika Trivedi <radhikatriv0712@gmail.com>
2025-05-21 00:55:27 +0530

    code for array tutorial in cpp

---
+++ arraysintro.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+    //declare
+    void printArray(int arr[],int size) {
+    cout<<"printing the array"<<endl;
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";    
+    
+      
+    }       
+    }     
+   int main(){
+    /*int number[15];
+    //accessing an array
+    cout<<"value at 14 index"<<number[14]<<endl;
+
+
+    int second[3]={5,7,11}; 
+    cout<<"value at 2 index"<<second[2]<<endl;
+     int secondsize = sizeof(second)/sizeof(int);
+     cout<<"size of second"<<secondsize<<endl; 
+     int third[15]={2,7};
+     int n=15;
+      printArray(third,15);*/
+    
+    
+    
+   /*int fourth[10]={0};
+    int k=10;
+    cout<<"printing an array"<<endl;
+    for(int i=0;i<k;i++){
+        cout<<fourth[i]<<" ";
+    } 
+
+
+    int fifth[10]={1};
+    int h=10;
+    cout<<"printing an array"<<endl;
+    for(int i=0;i<<h;i++){
+        cout<<fifth[i]<<" ";
+    }*/
+
+    char ch[5]={'a','b','c','d','e'};
+       cout<<"everything is fine"<<endl;
+       cout<<ch[2]<<endl;
+    
+      
+    return 0;
+}
